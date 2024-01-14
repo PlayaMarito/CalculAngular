5493d0b3bb9ff7d3dd8239eb0a5a6702d56c5116
PlayaMarito <1000aromascafe@gmail.com>
2024-01-13 21:25:37 -0500

    Envío 21 hs - aun sin funcionar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,16 @@
-import { RouterModule, Route } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'; // Cambio aquí: 'Route' a 'Routes'
 import { CalculaapComponent } from './calculaap/calculaap.component';
 import { NgModule } from '@angular/core';
 
-
-
-export const routes: Routes = [
+const routes: Routes = [ // Cambio aquí: 'Route' a 'Routes'
     {
         path: '',
-        component: CalculaapComponent
+        component: CalculaapComponent, 
     }
 ];
 
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/calculaap/calculaap.component.ts
+++ src/app/calculaap/calculaap.component.ts
@@ -1,18 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calculaap',
-  standalone: true,
-  imports: [FormsModule, 
-    CommonModule]
-    @Component({
-      selector: 'app-calculaap',
-      templateUrl: './calculaap.component.html',
-      styleUrls: ['./calculaap.component.css']
+  templateUrl: './calculaap.component.html',
+  styleUrls: ['./calculaap.component.css'],
+  standalone: true, // Esta propiedad no es necesaria
+  imports: [FormsModule, CommonModule]
 })
 export class CalculaapComponent {
-  numero1: any
-  numero2: any 
+  numero1: any;
+  numero2: any;
   resultado: number = 0;
+
+  sumar() {
+    this.resultado = Number(this.numero1) + Number(this.numero2);
+  }
+}
